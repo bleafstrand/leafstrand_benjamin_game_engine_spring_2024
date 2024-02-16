377b15261777de7fc4a5b0c29533a9449a8f17fc
bleafstrand <160270093+bleafstrand@users.noreply.github.com>
2024-02-16 10:19:52 -0800

    Hello Github

---
+++ setting.py
@@ -0,0 +1,19 @@
+# This file was created by: Benjamin Leafstrand
+TITLE = "Bens Epic Adventure"
+
+FPS = 60
+
+PLAYER_SPEED = 300
+#this is 32 grids wide
+WIDTH = 1024
+#this si 24 grids high
+HEIGHT = 768
+TILESIZE = 32
+
+SKYBLUE = (111, 186, 234)
+
+LIGHTGREY = (100,100,100)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+BGCOLOR = RED
